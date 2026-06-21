c9a5526fa9680949399acea417c041637d9f14cb
Hieu Ngo <sunflower.learner@gmail.com>
2026-06-21 11:42:58 +0700

    Add task numbering

--- todo.py
+++ todo.py
@@ -5,8 +5,13 @@ tasks = [
     "Read book"
 ]
 
+
 for task in tasks:
     print(task)
 
+for i, task in enumerate(tasks, start=1):
+    print(f"{i}. {task}")
+
 print()
 print(f"Total tasks: {len(tasks)}")
+
